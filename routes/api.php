ab9fe13b70747aa9140320198a35f5ad209bc1f6
root <root@vmi2209043.contaboserver.net>
2024-10-29 13:12:26 +0100

    Added API support with artisan

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
-// Ваш маршрути
 Route::get('/cars', [CarController::class, 'index']);
 Route::post('/cars', [CarController::class, 'store']);
 Route::put('/cars/{id}', [CarController::class, 'update']);
-Route::delete('/cars/{id}', [CarController::class, 'destroy']);
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');

c32ab0ac8727893e92e7abb0c16fccdf818e9a77
root <root@vmi2209043.contaboserver.net>
2024-10-29 13:26:37 +0100

    Added validation for fields

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use Illuminate\Http\Request;
+use Purifier; 
 
 class CarController extends Controller
 {
     public function index(Request $request)
     {
         if ($request->has(['license_plate', 'brand'])) {
-            $car = Car::where('license_plate', $request->license_plate)
-                        ->where('brand', $request->brand)
+            $car = Car::where('license_plate', strip_tags($request->license_plate)) 
+                        ->where('brand', strip_tags($request->brand)) 
                         ->first();
 
             if ($car) {
@@ -35,12 +36,20 @@ class CarController extends Controller
 
     public function store(Request $request)
     {
+        $this->validateInput($request->all());
+
         $validatedData = $request->validate([
             'owner' => 'required|string|max:40',
             'brand' => 'required|string|max:40',
             'license_plate' => 'required|string|max:8|unique:cars',
             'color' => 'required|string|max:40',
         ]);
+
+        
+        $validatedData['owner'] = strip_tags($validatedData['owner']);
+        $validatedData['brand'] = strip_tags($validatedData['brand']);
+        $validatedData['license_plate'] = strip_tags($validatedData['license_plate']);
+        $validatedData['color'] = strip_tags($validatedData['color']);
 
         $car = Car::create($validatedData);
         return response()->json([
@@ -60,6 +69,8 @@ class CarController extends Controller
             ], 404);
         }
 
+        $this->validateInput($request->all());
+
         $validatedData = $request->validate([
             'owner' => 'sometimes|required|string|max:40',
             'brand' => 'sometimes|required|string|max:40',
@@ -67,10 +78,32 @@ class CarController extends Controller
             'color' => 'sometimes|required|string|max:40',
         ]);
 
+      
+        $validatedData['owner'] = strip_tags($validatedData['owner']);
+        $validatedData['brand'] = strip_tags($validatedData['brand']);
+        $validatedData['license_plate'] = strip_tags($validatedData['license_plate']);
+        $validatedData['color'] = strip_tags($validatedData['color']);
+
         $car->update($validatedData);
         return response()->json([
             'status' => 200,
             'message' => 'Car updated successfully'
         ]);
+    }
+
+    private function validateInput($input)
+    {
+        $brandRegex = '/^[A-Za-z0-9 ]{1,40}$/'; 
+        $licensePlateRegex = '/^[A-Za-z0-9]{1,8}$/'; 
+        $colorRegex = '/^[A-Za-z ]{1,40}$/'; 
+        $ownerRegex = '/^[A-Za-z ]{1,40}$/'; 
+
+        if (!isset($input['brand'], $input['license_plate'], $input['owner'], $input['color'])
+            || !preg_match($brandRegex, $input['brand'])
+            || !preg_match($licensePlateRegex, $input['license_plate'])
+            || !preg_match($colorRegex, $input['color'])
+            || !preg_match($ownerRegex, $input['owner'])) {
+            return response()->json(['error' => 'Invalid input data'], 400)->send();
+        }
     }
 }

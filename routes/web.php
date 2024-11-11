2dea468328c40df2117f1c10c20c274e7f87e06b
root <root@vmi2209043.contaboserver.net>
2024-11-11 13:09:57 +0100

    Added Laravel Breeze to project

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CarController;
-
-//API controllers
-//Route::get('/api/cars', [CarController::class, 'index']);
-//Route::post('/api/cars', [CarController::class, 'store']);
-//Route::put('/api/cars/{code}', [CarController::class, 'update']);
-
 
 Route::get('/', function () {
-    return view('cars');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

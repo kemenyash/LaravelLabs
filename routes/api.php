4a0ef423d487d299ffc4508365b80ed3fa935f89
root <root@vmi2209043.contaboserver.net>
2024-11-16 19:06:46 +0100

    Added missed protection

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
+// Apply middleware to the entire route group
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/cars', [CarController::class, 'index']);
     Route::post('/cars', [CarController::class, 'store']);
     Route::put('/cars/{id}', [CarController::class, 'update']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
-
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
